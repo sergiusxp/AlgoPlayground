8a4f6b8ba73e4038381f14c41baf9a92a5ffaacc
Sergio Meloni <sergio.meloni@deltatre.it>
2020-09-13 16:54:29 +0200

    Menu inserted + middle node

---
+++ AlgoPlayground.Algorithms/LinkedLists/MiddleNode.cs
@@ -0,0 +1,31 @@
+﻿using AlgoPlayground.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoPlayground.Algorithms.LinkedLists
+{
+    public class MiddleNode<T>
+    {
+        private MyLinkedList<T> _list;
+
+        public void SetList(MyLinkedList<T> list)
+        {
+            _list = list;
+        }
+
+        public MyLinkedList<T> DeleteMiddleNode(MyLinkedList<T> nodeToDelete)
+        {
+            if (nodeToDelete == null || nodeToDelete.Next == null)
+            {
+                return null;
+            }
+
+            MyLinkedList<T> next = nodeToDelete.Next;
+            nodeToDelete.Data = next.Data;
+            nodeToDelete.Next = next.Next;
+
+            return _list;
+        }
+    }
+}
--- AlgoPlayground.Algorithms/Program.cs
+++ AlgoPlayground.Algorithms/Program.cs
@@ -10,30 +10,100 @@ namespace AlgoPlayground.Algorithms
     {
         static void Main(string[] args)
         {
-            #region LL -Remove Duplicates
-            var rd = new RemoveDuplicates<int>();
-            
-            rd.SetList(Mocks.GetIntList()); // 2 3 4 8 6 9
-            var list = rd.RemoveDup();
+            var actions = new Dictionary<string, List<string>>()
+            {
+                {
+                    "Linked List",
+                    new List<string>()
+                    {
+                        "Remove duplicates",
+                        "Find loop",
+                        "Find the kth to last element",
+                        "Delete middle node"
+                    }
+                }
+            };
 
-            rd.SetList(Mocks.GetIntList()); // 2 3 4 8 6 9
-            var listNoBuffer = rd.RemoveDupNoBufferAllowed();
-            #endregion
+            int myChose = -1;
+            while (myChose != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("MENU\n");
+                int index = 1;
+                foreach (var action in actions)
+                {
+                    Console.WriteLine("=== " + action.Key);
+                    foreach (var l in action.Value)
+                    {
+                        Console.WriteLine(" " + index++ + ") " + l);
+                    }
+                }
 
-            #region LL - Find Loop
-            var find = new FindLoop<int>();
-            find.SetList(Mocks.GetLoopIntList());
-            var loop = find.Find();
-            #endregion
+                Console.WriteLine("0) To exit\n\nWhat you chose? ");
+                string c = Console.ReadLine();
+                if (!Int32.TryParse(c, out myChose)) continue;
 
-            #region LL - Finding the Kth Element
-            var kth = new KthToLast<int>();
-            kth.SetList(LinkedListHelper.GetRandomLinkedListInt(8));
-            kth.Kth(1); // 1 means the last element
-            kth.Kth(2); // 2 means the last but one
-            #endregion
+                switch(myChose)
+                {
+                    case 1:
+                        #region LL - Remove Duplicates
+                        var rd = new RemoveDuplicates<int>();
+                        var listM = Mocks.GetIntList();
+                        Console.WriteLine("List before");
+                        LinkedListHelper.PrintList<int>(listM);
 
-            Console.ReadLine();
+                        rd.SetList(listM); // 2 3 4 8 6 9
+                        var list = rd.RemoveDup();
+
+                        rd.SetList(Mocks.GetIntList()); // 2 3 4 8 6 9
+                        var listNoBuffer = rd.RemoveDupNoBufferAllowed();
+
+                        LinkedListHelper.PrintList<int>(listNoBuffer);
+
+                        #endregion
+                        break;
+
+                    case 2:
+                        #region LL - Find Loop
+                        var find = new FindLoop<int>();
+                        var listL = Mocks.GetLoopIntList();
+                        find.SetList(listL);
+                        var loop = find.Find();
+
+                        Console.WriteLine("Loop found: " + loop.Data);
+
+                        #endregion
+                        break;
+
+                    case 3:
+                        #region LL - Finding the Kth to last Element
+                        var kth = new KthToLast<int>();
+                        kth.SetList(LinkedListHelper.GetRandomLinkedListInt(8));
+                        kth.Kth(1); // 1 means the last element
+                        kth.Kth(2); // 2 means the last but one
+                        #endregion
+                        break;
+
+                    case 4:
+                        #region LL - Delete middle node
+                        var del = new MiddleNode<int>();
+                        var ll = new List<int>() { 2, 5, 9, 2, 4 }.ConvertListToLinkedList();
+                        del.SetList(ll);
+
+                        Console.WriteLine("List before");
+                        LinkedListHelper.PrintList<int>(ll);
+
+                        var listWithoutMiddle = del.DeleteMiddleNode(ll.Next.Next); // we delete 9
+
+                        Console.WriteLine("List after");
+                        LinkedListHelper.PrintList<int>(listWithoutMiddle);
+
+                        #endregion
+                        break;
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
